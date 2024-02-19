fe9c901da79b4572e9386508fb351928b82d27df
aryadeifiordi <arya.93.ar@gmail.com>
2024-02-19 17:27:01 +0100

    javascript

---
+++ asset/js/main.js
@@ -0,0 +1,58 @@
+// numero di kilometri 
+// età del passeggero
+
+
+// prezzo 0.21 euro al km 
+// sconto 20% minorenni 
+// sconto 40% over 65 
+
+// Quanti kilometri devi percorrere?
+
+
+// raccolta dati
+const TravelKm = prompt('How many km have to travel?');
+
+const Age = prompt('How old are you?');
+
+// stampa dati
+
+console.log('km')
+console.log(typeof TravelKm);
+console.log(TravelKm);
+console.log('age')
+console.log(typeof Age);
+console.log(Age);
+
+// calcolo 
+
+let price = 0.21, result; // prezzo standard
+
+
+// se minore di 18 anni = sconto 20% su prezzo standard 
+
+// se maggiore di 65 anni = sconto 40% su prezzo standard 
+
+if(Age < 18){
+    //sconto 20%
+    discount = ((TravelKm * price) * 0.2);
+    result = ((TravelKm * price) - discount);
+
+    
+} else {
+    //standard price
+    result = TravelKm * price;
+}
+
+if(Age > 65){
+  // sconto 40%
+  discount = ((TravelKm * price) * 0.4);
+  result = ((TravelKm * price) - discount);
+
+}
+
+console.log('il prezzo del tuo biglietto: ' + result); 
+
+// se sono 17 km * 0,21 = 3.57 + sconto 2
+
+var numb = 12312214.124124124;
+numb = numb.toFixed(2);
